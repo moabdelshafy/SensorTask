5d37222c1662e61daf2ca013979d0ea91b81da48
ESRINEA\mohamed.abdelshafy <mohamed.abdelshafy@qsitint.com>
2023-06-01 01:40:36 +0300

    First Commit to Sensor Task

---
+++ src/main/java/com/sensor/SensorTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorTaskApplication.class, args);
+	}
+
+}
