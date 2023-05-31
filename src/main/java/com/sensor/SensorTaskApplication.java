c444c9032ccb139680156efa3f0e287721c30d8b
ESRINEA\mohamed.abdelshafy <mohamed.abdelshafy@qsitint.com>
2023-06-01 02:18:33 +0300

    added properties and structer of app

--- src/main/java/com/sensor/SensorTaskApplication.java
+++ src/main/java/com/sensor/SensorTaskApplication.java
@@ -2,8 +2,10 @@ package com.sensor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SensorTaskApplication {
 
 	public static void main(String[] args) {
